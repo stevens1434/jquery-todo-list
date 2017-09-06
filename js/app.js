11784dcbee950e5757daa5f89c9af017f7e3f4aa
AJ <stevens1434@AJs-MacBook-Air.local>
2017-09-06 09:23:59 -0700

    fixed delete button, added CSS

--- js/app.js
+++ js/app.js
@@ -1,28 +1,24 @@
 $(document).ready(function() {
-  var textBox;
-  var addButton = document.getElementById('#button');
-  var wholeList = document.getElementById('#wholelist');
+	var textBox;
+	var addButton = document.getElementById('#button');
+	var wholeList = document.getElementById('#wholelist');
 
-  $('#button').on('click', function() {
+$('#button').on('click', function() {
 	textBox = $('#textbox').val();
-	$('#wholelist').append('<li>' + textBox + '</li>');
-	$('#wholelist').append('<input type="button" value="X" class="deletebutton">');
-	jQuery('#textbox').focus(function()
-	{
-  	$(this).val('');
+	var newItem = $('<li>').append(textBox);
+	// $('#wholelist').append('<li>' + textBox + '</li>');
+	$(newItem).append('<input type="button" value="X" class="deletebutton">');
+	$("#wholelist").append(newItem);
+	jQuery('#textbox').focus(function()	{
+	$(this).val('');
+	});
 });
-  });
 
 $(function() {
-  $("#textbox").focus();
+	$("#textbox").focus();
 });
 
-  $('#wholelist').on('click', "li", function() {
-	$("li").remove(this.li); });
-	$('li').remove('#deletebutton');
+$('#wholelist').on('click', "input", function(e) {
+	$(e.target).parent().remove();
+	});
 });
-
-
-// i think i need to have teh DOM add a specific "id" tag to each NEW "li" item and then reference taht to delete it.
-
-// also need to add a function to allow me to start typing right away.
