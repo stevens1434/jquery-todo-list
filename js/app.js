5e7bf93cf190c809d31e91bd52ee86a60c687107
AJ <stevens1434@AJs-MacBook-Air.local>
2017-09-05 23:05:27 -0700

    added notes and focus function for textbox

--- js/app.js
+++ js/app.js
@@ -1,26 +1,28 @@
 $(document).ready(function() {
+  var textBox;
+  var addButton = document.getElementById('#button');
+  var wholeList = document.getElementById('#wholelist');
 
-
-var textBox;
-var addButton = document.getElementById("#button");
-var wholeList = document.getElementById("#wholelist");
-
-$("#button").on('click', function() {
+  $('#button').on('click', function() {
 	textBox = $('#textbox').val();
-	$("#wholelist").append('<li>' + textBox + "</li>");
-	$("#wholelist").append('<input type="button" value="X" class="deletebutton">');
-	jQuery("#textbox").focus( function()
-	{ 
-  	$(this).val(""); 
+	$('#wholelist').append('<li>' + textBox + '</li>');
+	$('#wholelist').append('<input type="button" value="X" class="deletebutton">');
+	jQuery('#textbox').focus(function()
+	{
+  	$(this).val('');
+});
+  });
+
+$(function() {
+  $("#textbox").focus();
 });
 
-});
-$("#wholelist").on('click',function() { 
-	$(this).remove()});
-    $("#deletebutton").remove("li");
-
+  $('#wholelist').on('click', "li", function() {
+	$("li").remove(this.li); });
+	$('li').remove('#deletebutton');
 });
 
 
+// i think i need to have teh DOM add a specific "id" tag to each NEW "li" item and then reference taht to delete it.
 
-
+// also need to add a function to allow me to start typing right away.
